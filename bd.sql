ea1c9dfe4065908a2e8cd0ca1c75e205b7d604ab
Amal <amal.amami30@gmail.com>
2025-05-06 12:02:52 +0100

    Connexion vulnérable aux injections SQL

---
+++ bd.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS not_secure;
+USE not_secure;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(30) NOT NULL,
+    prenom VARCHAR(40) NOT NULL ,
+    email TEXT NOT NULL,
+    pass TEXT NOT NULL 
+   
+);
+
+INSERT INTO user (nom ,prenom ,email, pass) VALUES
+('Amal', 'Dadda Amami ','amal@gmail.com','amal123'),
